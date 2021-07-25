3ca6d22efa8d403deb9b3d86251bec5af7be52ba
byCrypex <pixeldev02@gmail.com>
2021-07-25 20:27:28 +0200

    Improve build system

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     java
+    maven
     id("net.minecrell.plugin-yml.bukkit") version "0.4.0"
+    id("com.github.johnrengelman.shadow") version "4.0.4"
+    `maven-publish`
+    `java-library`
+
 }
 
 group = "net.trainingsoase"
@@ -8,9 +13,34 @@ version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
+    maven {
+        url = uri("https://papermc.io/repo/repository/maven-public/")
+    }
+    maven {
+        url = uri("https://gitlab.madfix.de/api/v4/groups/64/-/packages/maven")
+        name = "GitLab"
+        credentials(HttpHeaderCredentials::class) {
+            name = "Job-Token"
+            value = System.getenv("CI_JOB_TOKEN")
+        }
+        credentials(HttpHeaderCredentials::class) {
+            name = "Private-Token"
+            value = property("gitlabToken") as String
+        }
+        authentication {
+            create<HttpHeaderAuthentication>("header")
+        }
+    }
+
 }
 
 dependencies {
+
+    compileOnly("net.trainingsoase:OaseAPI-Spigot:0.0.0-SNAPSHOT")
+    compileOnly("org.github.paperspigot:paperspigot-api:1.8.8-R0.1-SNAPSHOT")
+
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
@@ -18,6 +48,32 @@ dependencies {
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = "bedwars" // Only lowercase!!!!!
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://gitlab.madfix.de/api/v4/projects/176/packages/maven")
+            name = "GitLab"
+            credentials(HttpHeaderCredentials::class) {
+                name = "Job-Token"
+                value = System.getenv("CI_JOB_TOKEN")
+            }
+            authentication {
+                create<HttpHeaderAuthentication>("header")
+            }
+        }
+    }
+}
+
 
 bukkit {
     name = "Bedwars"
@@ -28,4 +84,13 @@ bukkit {
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+}
+tasks {
+    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+        // archiveBaseName.set("shadow")
+        mergeServiceFiles()
+    }
+    build {
+        dependsOn(shadowJar)
+    }
 }
