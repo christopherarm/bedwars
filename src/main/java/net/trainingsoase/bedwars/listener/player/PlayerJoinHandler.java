6a9b757aae545a65ae26c6af23d1cff8b479ce0e
byCrypex | Chris <byCrypex@trainingsoase.net>
2021-07-29 01:18:24 +0200

    Add join- and quitmessage

--- src/main/java/net/trainingsoase/bedwars/listener/player/PlayerJoinHandler.java
+++ src/main/java/net/trainingsoase/bedwars/listener/player/PlayerJoinHandler.java
@@ -5,14 +5,21 @@ import net.trainingsoase.api.player.IOasePlayer;
 import net.trainingsoase.bedwars.Bedwars;
 import net.trainingsoase.bedwars.phase.LobbyPhase;
 import net.trainingsoase.data.OaseAPIImpl;
+import net.trainingsoase.data.model.OasePlayer;
 import net.trainingsoase.hopjes.api.phase.LinearPhaseSeries;
 import net.trainingsoase.hopjes.api.phase.TimedPhase;
+import net.trainingsoase.oreo.scoreboard.ScoreboardAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scoreboard.DisplaySlot;
+
+import java.util.HashMap;
+
+import static net.trainingsoase.oreo.scoreboard.ScoreboardAPI.INSTANCE;
 
 /**
  * @author byCrypex
@@ -26,27 +33,46 @@ public class PlayerJoinHandler implements Listener {
 
     private final LinearPhaseSeries<TimedPhase> phaseSeries;
 
+    private final HashMap<String, Integer> sidebar = new HashMap<>();
+
     public PlayerJoinHandler(Bedwars bedwars, LinearPhaseSeries<TimedPhase> phaseSeries) {
         this.bedwars = bedwars;
         this.phaseSeries = phaseSeries;
+
+        sidebar.put("§8§m----------------", 12);
+        sidebar.put("§7", 11);
+        sidebar.put(" §8➥ §7", 10);
+        sidebar.put(" §b", 9);
+        sidebar.put("§b■ §7Map:", 8);
+        sidebar.put(" §8➥ §b", 7);
+        sidebar.put(" §c", 6);
+        sidebar.put("§e■ §7Coins:", 5);
+        sidebar.put(" §8➥ §e", 4);
+        sidebar.put(" §d", 3);
+        sidebar.put("§a■ §7Ranking:", 2);
+        sidebar.put(" §8➥ §a", 1);
+        sidebar.put("         ", 0);
     }
 
     @EventHandler
     public void handleJoin(final PlayerJoinEvent event) {
         event.setJoinMessage(null);
         final Player player = event.getPlayer();
+        final IOasePlayer oasePlayer = OaseAPIImpl.INSTANCE.getPlayerExecutor().getOnlinePlayer(player.getUniqueId());
 
         if (phaseSeries.getCurrentPhase() instanceof LobbyPhase) {
             var lobbyPhase = (LobbyPhase) phaseSeries.getCurrentPhase();
             lobbyPhase.checkStartCondition();
 
-            var cache = new MessageCache(bedwars.getLanguageProvider(), "join_message", player.getDisplayName());
-
-            for (IOasePlayer oasePlayer : OaseAPIImpl.INSTANCE.getPlayerExecutor().getCurrentOnlinePlayers()) {
-                bedwars.getLanguageProvider().sendMessage(Bukkit.getPlayer(oasePlayer.getUUID()), oasePlayer, cache.getMessage(oasePlayer.getLocale()));
+            for (IOasePlayer iOasePlayer : OaseAPIImpl.INSTANCE.getPlayerExecutor().getCurrentOnlinePlayers()) {
+                bedwars.getLanguageProvider().sendMessage(Bukkit.getConsoleSender(), iOasePlayer, "join_message",
+                        player.getDisplayName(),
+                        Bukkit.getOnlinePlayers().size(),
+                        bedwars.getMode().getPlayers());
             }
 
             setupPlayer(player);
+            setupScoreboard(player, oasePlayer);
         }
     }
 
@@ -61,5 +87,15 @@ public class PlayerJoinHandler implements Listener {
         player.setGameMode(GameMode.ADVENTURE);
         player.setAllowFlight(false);
         player.setFlying(false);
+    }
+
+    private void setupScoreboard(Player player, IOasePlayer oasePlayer) {
+        INSTANCE.setSidebar(player, DisplaySlot.SIDEBAR, "§c§lBedwars §8§l︳§eLobby", sidebar);
+        INSTANCE.updateTeam(player, "Team", " §8➥ §7", "", "§4✖");
+        INSTANCE.updateTeam(player, "PreTeam", "§7", "§7■ Team:", "");
+        INSTANCE.updateTeam(player, "Coins", " §8➥ §e", "§e", "" + oasePlayer.getCoins());
+        INSTANCE.updateTeam(player, "Ranking", " §8➥ §a", "§a", "#1");
+        INSTANCE.updateTeam(player, "Map", " §8➥ §b", "§b", "§bTest");
+
     }
 }
--- src/main/java/net/trainingsoase/bedwars/listener/player/PlayerQuitHandler.java
+++ src/main/java/net/trainingsoase/bedwars/listener/player/PlayerQuitHandler.java
@@ -1,9 +1,12 @@
 package net.trainingsoase.bedwars.listener.player;
 
+import net.trainingsoase.api.player.IOasePlayer;
 import net.trainingsoase.bedwars.Bedwars;
 import net.trainingsoase.bedwars.phase.LobbyPhase;
+import net.trainingsoase.data.OaseAPIImpl;
 import net.trainingsoase.hopjes.api.phase.LinearPhaseSeries;
 import net.trainingsoase.hopjes.api.phase.TimedPhase;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,9 +20,12 @@ import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerQuitHandler implements Listener {
 
+    private final Bedwars bedwars;
+
     private final LinearPhaseSeries<TimedPhase> phaseSeries;
 
     public PlayerQuitHandler(Bedwars bedwars, LinearPhaseSeries<TimedPhase> phaseSeries) {
+        this.bedwars = bedwars;
         this.phaseSeries = phaseSeries;
     }
 
@@ -30,6 +36,13 @@ public class PlayerQuitHandler implements Listener {
         if (phaseSeries.getCurrentPhase() instanceof LobbyPhase) {
             var lobbyPhase = (LobbyPhase) phaseSeries.getCurrentPhase();
             lobbyPhase.checkStopCondition();
+
+            for (IOasePlayer iOasePlayer : OaseAPIImpl.INSTANCE.getPlayerExecutor().getCurrentOnlinePlayers()) {
+                bedwars.getLanguageProvider().sendMessage(Bukkit.getConsoleSender(), iOasePlayer, "quit_message",
+                        player.getDisplayName(),
+                        Bukkit.getOnlinePlayers().size() - 1,
+                        bedwars.getMode().getPlayers());
+            }
             return;
         }
 
